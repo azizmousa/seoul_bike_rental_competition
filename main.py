bfd3310797a3954d5b24a0a18039fb1b693a7047
azizmousa <azizmousa1010@gmail.com>
2021-12-16 17:24:33 +0200

    create upload test file endpoint

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import os
+import datetime
+from fastapi import FastAPI, UploadFile, File
 import model
 
 app = FastAPI()
@@ -9,7 +11,19 @@ async def home():
     return {"hello": "aziz"}
 
 
-@app.get('/predict')
-async def predict():
+@app.get('/predict_test')
+async def predict_test():
     return {"test_predictions": list(model.predict('seoul-bike-rental-ai-pro-iti/test.csv'))}
+
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    os.makedirs('uploaded', exist_ok=True)
+    file_name = str(datetime.datetime.now()) + f"_{file.filename}"
+    file_path = f"uploaded/{file_name}"
+    with open(file_path, 'wb') as out_file:
+        content = await file.read()
+        out_file.write(content)
+    return {"predictions": list(model.predict(file_path))}
+
 
